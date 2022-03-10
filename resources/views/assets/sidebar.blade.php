aafd301dae58a10a990a318a7b4c742456dd401f
santosh kumar pandit <skpandit659@gmail.com>
2022-03-10 23:09:15 +0545

    first

---
+++ resources/views/assets/sidebar.blade.php
@@ -0,0 +1,56 @@
+<div class="left-sidebar-pro">
+    <nav id="sidebar" class="">
+        <div class="sidebar-header">
+            <a href="index.html"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+            <strong><a href="index.html"><img src="img/logo/logosn.png" alt="" /></a></strong>
+        </div>
+        <div class="left-custom-menu-adp-wrap comment-scrollbar">
+            <nav class="sidebar-nav left-sidebar-menu-pro">
+                <ul class="metismenu" id="menu1">
+                    <li class="active">
+                        <a title="Landing Page" href="events.html" aria-expanded="false"><img src="https://img.icons8.com/office/22/000000/dashboard.png"/> <span class="mini-click-non">Dashboard</span></a>
+
+                    </li>
+                    {{-- <li>
+                        <a title="Landing Page" href="events.html" aria-expanded="false"><span class="educate-icon educate-event icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Event</span></a>
+                    </li>  --}}
+                    <li><a title="Landing Page" href="events.html" aria-expanded="false"><img src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/25/000000/external-electricity-energy-kiranshastry-lineal-kiranshastry.png"/> <span class="mini-click-non">Electricity</span></a></li>
+                    <li><a title="Landing Page" href="events.html" aria-expanded="false"><img src="https://img.icons8.com/external-outline-agus-raharjo/22/000000/external-furniture-furniture-and-household-outline-agus-raharjo-10.png"/><span class="mini-click-non">Furniture</span></a></li>
+                    <li><a title="Landing Page" href="events.html" aria-expanded="false"><img src="https://img.icons8.com/ios/22/000000/hamburger.png"/> <span class="mini-click-non">Lunch</span></a></li>
+                    <li><a title="Landing Page" href="events.html" aria-expanded="false"><img src="https://img.icons8.com/ios/22/000000/box-important--v1.png"/> <span class="mini-click-non">Miscellaneous</span></a></li>
+                    <li><a title="Landing Page" href="events.html" aria-expanded="false"><img src="https://img.icons8.com/external-line-rakhmat-setiawan/24/000000/external-office-office-and-work-room-line-rakhmat-setiawan-2.png"/> <span class="mini-click-non">Office Expenses</span></a></li>
+                    <li><a title="Landing Page" href="events.html" aria-expanded="false"><img src="https://img.icons8.com/ios/23/000000/sell-property--v1.png"/> <span class="mini-click-non">Office Rent</span></a></li>
+                    <li><a title="Landing Page" href="events.html" aria-expanded="false"><img src="https://img.icons8.com/external-becris-lineal-becris/24/000000/external-salary-customer-loyalty-program-becris-lineal-becris.png"/></span> <span class="mini-click-non">Salary</span></a></li>
+                    <li><a title="Landing Page" href="events.html" aria-expanded="false"><img src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/24/000000/external-stationery-back-to-school-vitaliy-gorbachev-lineal-vitaly-gorbachev.png"/> <span class="mini-click-non">Stationery</span></a></li>
+                    <li><a title="Landing Page" href="events.html" aria-expanded="false"><img src="https://img.icons8.com/ios/26/000000/passenger-with-baggage.png"/></span> <span class="mini-click-non">Travel Expenses</span></a></li>
+                    <li><a title="Landing Page" href="events.html" aria-expanded="false"><img src="https://img.icons8.com/external-icongeek26-outline-icongeek26/25/000000/external-vehicle-war-icongeek26-outline-icongeek26.png"/></span> <span class="mini-click-non">Vehicle Fair</span></a></li>
+                    <li><a title="Landing Page" href="events.html" aria-expanded="false"><img src="https://img.icons8.com/ios/21/000000/maintenance.png"/></span> <span class="mini-click-non">Vehicle Maintenance</span></a></li>
+                    <li><a title="Landing Page" href="events.html" aria-expanded="false"><img src="https://img.icons8.com/external-smashingstocks-detailed-outline-smashing-stocks/25/000000/external-cash-social-marketing-smashingstocks-detailed-outline-smashing-stocks.png"/> <span class="mini-click-non">Cash Book</span></a></li>
+                
+                    <li>
+                        <a class="has-arrow" href="mailbox.html" aria-expanded="false"><img src="https://img.icons8.com/ios/25/000000/personal-trainer.png"/> <span class="mini-click-non">Personals</span></a>
+                        <ul class="submenu-angle app-mini-nb-dp" aria-expanded="false">
+                            <li><a title="Notifications" href="notifications.html"><span class="mini-sub-pro">Notifications</span></a></li>
+                            <li><a title="Alerts" href="alerts.html"><span class="mini-sub-pro">Alerts</span></a></li>
+                            <li><a title="Modals" href="modals.html"><span class="mini-sub-pro">Modals</span></a></li>
+                            <li><a title="Buttons" href="buttons.html"><span class="mini-sub-pro">Buttons</span></a></li>
+                            <li><a title="Tabs" href="tabs.html"><span class="mini-sub-pro">Tabs</span></a></li>
+                            <li><a title="Accordion" href="accordion.html"><span class="mini-sub-pro">Accordion</span></a></li>
+                        </ul>
+                    </li>
+                    <li id="removable" style="margin-bottom: 40px">
+                        <a class="has-arrow" href="#" aria-expanded="false"><img src="https://img.icons8.com/external-outline-wichaiwi/19/000000/external-company-business-continuity-plan-outline-wichaiwi-3.png"/> <span class="mini-click-non">Companies</span></a>
+                        <ul class="submenu-angle page-mini-nb-dp" aria-expanded="false">
+                            <li><a title="Login" href="login.html"><span class="mini-sub-pro">Login</span></a></li>
+                            <li><a title="Register" href="register.html"><span class="mini-sub-pro">Register</span></a></li>
+                            <li><a title="Lock" href="lock.html"><span class="mini-sub-pro">Lock</span></a></li>
+                            <li><a title="Password Recovery" href="password-recovery.html"><span class="mini-sub-pro">Password Recovery</span></a></li>
+                            <li><a title="404 Page" href="404.html"><span class="mini-sub-pro">404 Page</span></a></li>
+                            <li><a title="500 Page" href="500.html"><span class="mini-sub-pro">500 Page</span></a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </nav>
+</div>
